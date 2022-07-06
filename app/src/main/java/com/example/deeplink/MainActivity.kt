2c9461bac8efe31863b5619080eed136b5b6c0ea
Roman Geraskin <arizekaz@gmail.com>
2022-07-06 10:36:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/deeplink/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.deeplink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("TEST_TAG", "data = ${intent.data}")
+        Log.d("TEST_TAG", "dataString = ${intent.dataString}")
+    }
+}
